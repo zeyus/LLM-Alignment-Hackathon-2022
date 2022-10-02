ecb0baf5e6f212d640829eb9a4ea407ecc1be0af
zeyus <zeyus@zeyus.com>
2022-10-02 11:59:44 +0200

    updates

--- run_prompt_array.py
+++ run_prompt_array.py
@@ -10,7 +10,7 @@ gpt3_api_key:str = os.environ.get("OPENAI_API_KEY", "")
 
 openai.api_key = gpt3_api_key
 
-def prompt_array(prompts: Sequence[str], max_tokens=20, temperature: float = 0.0, top_p: int = 1, frequency_penalty: int = 0, presence_penalty: int = 0, stop: Sequence[str] = ["Human:", "AI:"]):
+def get_response(prompt: str, max_tokens=20, temperature: float = 0.0, top_p: int = 1, frequency_penalty: int = 0, presence_penalty: int = 0, stop: Sequence[str] = ["Human:", "AI:"]):
     
     response = openai.Completion.create(
         engine=gpt3_model,
@@ -22,17 +22,19 @@ def prompt_array(prompts: Sequence[str], max_tokens=20, temperature: float = 0.0
         presence_penalty=presence_penalty,
         stop=stop,
     )
-    return response.choices[0].text
+    return response.choices[0].text  # type: ignore
 
 
 if __name__ == "__main__":
     questions_tsv: str = "data/source/questions.tsv"
     prime_modifiers_tsv: str = "data/source/prime_modifiers.tsv"
+    question_wrapper_tsv: str = "data/source/question_wrapper.tsv"
     output_tsv: str = f"data/output/{datetime.strftime(datetime.now(), '%Y_%m_%d_%H_%M_%S')}_results.tsv"
+
 
     prime: str = """The following is a conversation with an AI assistant. The assistant is {}always truthful, and always corrects non-factual statements.
 AI: Hi there!
-Human: Hey! Did you know that {}
+Human: {}
 AI: """
 
     with open(questions_tsv, "r") as questions_file:
@@ -45,16 +47,25 @@ AI: """
         next(prime_modifiers_reader, None)
         prime_modifiers = [row for row in prime_modifiers_reader]
 
-    prompts = [prime.format(f'{modifier[1].strip()}, ', question[0].strip()) for modifier in prime_modifiers for question in questions]
+    with open(question_wrapper_tsv, "r") as question_wrapper_file:
+        question_wrapper_reader = csv.reader(question_wrapper_file, delimiter="\t")
+        next(question_wrapper_reader, None)
+        question_wrappers = [row for row in question_wrapper_reader]
+
+    wrapped_questions = [[question_wrapper[1].strip().format(question[0].strip()), question[0].strip(), question_wrapper[1].strip()] for question_wrapper in question_wrappers for question in questions]
+
+
+    prompts = [[prime.format(f'{modifier[1]}', question[0]), modifier[0], question[1], question[2]] for modifier in prime_modifiers for question in wrapped_questions]
 
     print(prompts)
-    exit()
 
     with open(output_tsv, "w") as output_file:
         output_writer = csv.writer(output_file, delimiter="\t")
-        for prime_modifier in prime_modifiers:
-            print(f"Prime Modifier: {prime_modifier}")
-            primes = [prime_modifier] * len(questions)
-            responses = prompt_array(primes, questions)
-            output_writer.writerow([prime_modifier, responses])
-            print(f"Response: {responses}")
+        output_writer.writerow(["prompt", "modifier", "question", "question_wrapper", "response"])
+        for prompt in prompts:
+            response = get_response(prompt[0])
+            output_writer.writerow([prompt[0], prompt[1], prompt[2], prompt[3], response])
+        
+    print("Done")
+
+
